40915996a3435abeaff019e1e1d63e19fb26ea67
r-4bb1t <r4bb1t.github@gmail.com>
2021-08-21 01:16:23 +0900

    refactor: 로직 파일 따로 분리

---
+++ src/view/main/hooks.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Profile } from "../../constant";
+import { DummyProfiles } from "../../dummy";
+
+export function useMain() {
+  const [profiles, setProfiles] = useState(DummyProfiles);
+  const [filteredProfiles, setFilteredProfiles] = useState(DummyProfiles);
+  const [tags, setTags] = useState([] as string[]);
+
+  const removeTag = (index: number) => {
+    const newTags = [...tags];
+    newTags.splice(index, 1);
+    setTags(newTags);
+  };
+
+  const addTag = async (e: any) => {
+    const val = e.target.value;
+    if (tags.length > 9) return;
+    if ((e.key === "Enter" || e.key === " ") && val) {
+      if (
+        tags.find((tag) => tag.replace(/\s/g, "") === val.replace(/\s/g, ""))
+      ) {
+        return;
+      }
+      e.target.value = "";
+      setTags([...tags, val.replace(/\s/g, "")]);
+    } else if (e.key === "Backspace" && !val) {
+      removeTag(tags.length - 1);
+    }
+  };
+
+  const applyFilter = () => {
+    setFilteredProfiles(
+      profiles.filter((profile) =>
+        tags.every((tag) =>
+          [
+            ...profile.hobby,
+            ...profile.interest,
+            ...profile.stack,
+            ...profile.etc,
+          ].includes(tag)
+        )
+      )
+    );
+  };
+
+  return { profiles, filteredProfiles, tags, removeTag, addTag, applyFilter };
+}
--- src/view/main/index.tsx
+++ src/view/main/index.tsx
@@ -1,6 +1,4 @@
 import * as S from "./styles";
-import { Profile } from "../../constant";
-import { DummyProfiles } from "../../dummy";
 import {
   Add,
   Email,
@@ -10,48 +8,10 @@ import {
   Search,
 } from "@material-ui/icons";
 import Header from "../../component/header";
-import { useState } from "react";
+import { useMain } from "./hooks";
 
 export default function Main() {
-  const [profiles, setProfiles] = useState(DummyProfiles);
-  const [filteredProfiles, setFilteredProfiles] = useState(DummyProfiles);
-  const [tags, setTags] = useState([] as string[]);
-  const removeTag = (index: number) => {
-    const newTags = [...tags];
-    newTags.splice(index, 1);
-    setTags(newTags);
-  };
-  const addTag = async (e: any) => {
-    const val = e.target.value;
-    if (tags.length > 9) return;
-    if ((e.key === "Enter" || e.key === " ") && val) {
-      if (
-        tags.find((tag) => tag.replace(/\s/g, "") === val.replace(/\s/g, ""))
-      ) {
-        return;
-      }
-      e.target.value = "";
-      setTags([...tags, val.replace(/\s/g, "")]);
-    } else if (e.key === "Backspace" && !val) {
-      removeTag(tags.length - 1);
-    }
-  };
-
-  const applyFilter = () => {
-    setFilteredProfiles(
-      profiles.filter((profile) =>
-        tags.every((tag) =>
-          [
-            ...profile.hobby,
-            ...profile.interest,
-            ...profile.stack,
-            ...profile.etc,
-          ].includes(tag)
-        )
-      )
-    );
-  };
-
+  const { filteredProfiles, tags, removeTag, addTag, applyFilter } = useMain();
   return (
     <S.Main>
       <Header />
